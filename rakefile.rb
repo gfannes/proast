f3b6a39d3ed8efac97ea56b590dfcd69ec2261ca
Geert Fannes <geert.fannes@gmail.com>
2020-04-19 18:47:19 +0200

    Added support for installing notcurses

--- rakefile.rb
+++ rakefile.rb
@@ -4,8 +4,30 @@ task :default do
     sh "rake -T"
 end
 
+file ".extern" do
+    puts "creating dir .extern"
+    FileUtils.mkdir(".extern")
+end
+file ".extern/notcurses/ok" => ".extern" do
+    Dir.chdir(".extern") do
+        FileUtils.rm_rf("notcurses")
+        sh "git clone https://github.com/dankamongmen/notcurses"
+        Dir.chdir("notcurses") do
+            FileUtils.mkdir("build")
+            Dir.chdir("build") do
+                sh "cmake .."
+                sh "make -j 4"
+                sh "sudo make install"
+            end
+            sh "touch ok"
+        end
+    end
+end
+
+task :dependencies => ".extern/notcurses/ok"
+
 desc "Build proast"
-task :build do
+task :build => :dependencies do
     sh "cook -g ninja -T c++.std=17 proast"
     sh "ninja -v"
 end
@@ -20,4 +42,10 @@ task :clear do
     FileUtils.rm_rf(".cook")
     FileUtils.rm_f("proast")
     FileUtils.rm_f("build.ninja")
+end
+
+desc "Remove everything"
+task :proper => :clear do
+    FileUtils.rm_rf(".extern")
+    sh "sudo rm -rf .extern/notcurses"
 end
