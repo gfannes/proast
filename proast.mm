b0dbb78d72fce1bc89f57db012173d7ddf76c114
Geert Fannes <geert.fannes@gmail.com>
2021-01-06 00:05:28 +0100

    Added mm

---
+++ proast.mm
@@ -0,0 +1,142 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="proast" FOLDED="false" ID="ID_1298256357" CREATED="1609883997784" MODIFIED="1609884060435" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="3.137">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="REQUIREMENTS" POSITION="right" ID="ID_1174909483" CREATED="1609884067703" MODIFIED="1609884246136">
+<edge COLOR="#ff0000"/>
+<node TEXT="Manage projects" ID="ID_1450988731" CREATED="1609884191019" MODIFIED="1609884256176">
+<node TEXT="personal" ID="ID_900442381" CREATED="1609884256182" MODIFIED="1609884258306"/>
+<node TEXT="professional" ID="ID_1818442386" CREATED="1609884258523" MODIFIED="1609884281500"/>
+</node>
+<node TEXT="Translate requirements into action plan" ID="ID_228574777" CREATED="1609884367441" MODIFIED="1609884449910">
+<node TEXT="do and improve" ID="ID_1398723160" CREATED="1609885749678" MODIFIED="1609885955337"/>
+</node>
+<node TEXT="Support for metadata" ID="ID_623773751" CREATED="1609884452298" MODIFIED="1609884480103">
+<node TEXT="priorities" ID="ID_401495486" CREATED="1609884481926" MODIFIED="1609884486434"/>
+<node TEXT="status" ID="ID_1280230212" CREATED="1609884486677" MODIFIED="1609884492457"/>
+<node TEXT="effort" ID="ID_1666650801" CREATED="1609884492733" MODIFIED="1609885267943"/>
+<node TEXT="importance" ID="ID_1440854000" CREATED="1609884735431" MODIFIED="1609884740668"/>
+<node TEXT="skills" ID="ID_1005428519" CREATED="1609885272511" MODIFIED="1609885281349">
+<node TEXT="aggregation towards root" ID="ID_747638724" CREATED="1609887238451" MODIFIED="1609887251790"/>
+<node TEXT="inheritance towards leafs" ID="ID_1569236559" CREATED="1609887255989" MODIFIED="1609887264245"/>
+</node>
+<node TEXT="start and deadline" ID="ID_880966657" CREATED="1609885294607" MODIFIED="1609885306793"/>
+<node TEXT="dependencies" ID="ID_268163253" CREATED="1609887272263" MODIFIED="1609887275423"/>
+<node TEXT="tags" ID="ID_944473687" CREATED="1609887660125" MODIFIED="1609887661658"/>
+</node>
+<node TEXT="Browse filesystem" ID="ID_916752492" CREATED="1609885026318" MODIFIED="1609885037403">
+<node TEXT="open" ID="ID_1399761680" CREATED="1609885045354" MODIFIED="1609886191083">
+<node TEXT="files" ID="ID_567265272" CREATED="1609886191089" MODIFIED="1609886192860"/>
+<node TEXT="shell" ID="ID_1556185998" CREATED="1609886193177" MODIFIED="1609886194618"/>
+</node>
+<node TEXT="copy/paste/move" ID="ID_518351930" CREATED="1609885057779" MODIFIED="1609886206631"/>
+</node>
+<node TEXT="Commit all data with standard version control" ID="ID_1246267302" CREATED="1609885078117" MODIFIED="1609885112215">
+<node TEXT="combine different root folders" ID="ID_1116900402" CREATED="1609885336641" MODIFIED="1609885345864"/>
+</node>
+<node TEXT="Export" ID="ID_1513048858" CREATED="1609885112675" MODIFIED="1609885114688">
+<node TEXT="Static html pages of content" ID="ID_98145426" CREATED="1609885115729" MODIFIED="1609885203571"/>
+<node TEXT="gnuplot gantt chart" ID="ID_674448761" CREATED="1609885204931" MODIFIED="1609885252762">
+<node TEXT="based on tree structure dependencies, metadata dependencies and priorities" ID="ID_1190048894" CREATED="1609887283030" MODIFIED="1609887344989"/>
+</node>
+</node>
+<node TEXT="Provide overview of data" ID="ID_1915311678" CREATED="1609885533273" MODIFIED="1609885740313">
+<node TEXT="todos" ID="ID_1578742226" CREATED="1609885542417" MODIFIED="1609885561753"/>
+<node TEXT="overview" ID="ID_1107211616" CREATED="1609885562858" MODIFIED="1609885565681"/>
+<node TEXT="details" ID="ID_1801567856" CREATED="1609885566367" MODIFIED="1609885567592"/>
+<node TEXT="filter" ID="ID_66242898" CREATED="1609886279637" MODIFIED="1609886281003"/>
+</node>
+<node TEXT="Describe markdown and metadata format" ID="ID_1844729152" CREATED="1609887464088" MODIFIED="1609887488684"/>
+<node TEXT="Store and refer external documents" ID="ID_1840071978" CREATED="1609887631187" MODIFIED="1609887655453"/>
+<node TEXT="Search in data" ID="ID_215978941" CREATED="1609887665298" MODIFIED="1609887673034">
+<node TEXT="structured search using tags etc." ID="ID_280892081" CREATED="1609887673650" MODIFIED="1609887684162"/>
+</node>
+</node>
+<node TEXT="TYPES" POSITION="right" ID="ID_1052188331" CREATED="1609886801310" MODIFIED="1609886805099">
+<edge COLOR="#0000ff"/>
+<node TEXT="Deliverable" ID="ID_1813016627" CREATED="1609886806631" MODIFIED="1609886812108">
+<node TEXT="Set of requirements that should be completed before something becomes usefull for a customer" ID="ID_487253661" CREATED="1609887036227" MODIFIED="1609887101603"/>
+</node>
+<node TEXT="Requirement" ID="ID_141553463" CREATED="1609886813037" MODIFIED="1609886818588">
+<node TEXT="Single-focussed description, free from early technical decisions" ID="ID_1800286127" CREATED="1609887107538" MODIFIED="1609887166864"/>
+</node>
+<node TEXT="Design" ID="ID_1526025968" CREATED="1609886822650" MODIFIED="1609886825187">
+<node TEXT="Additional constraints on the Requirements to reduce the amount of different approaches, keeping non-functional requirements into account (like maintenance, cost, ...)" ID="ID_779230305" CREATED="1609886826478" MODIFIED="1609886969455"/>
+</node>
+<node TEXT="Feature" ID="ID_1531829154" CREATED="1609887169596" MODIFIED="1609887171543">
+<node TEXT="?" ID="ID_788252931" CREATED="1609887172512" MODIFIED="1609887173443"/>
+</node>
+<node TEXT="Task" ID="ID_1203069738" CREATED="1609886818964" MODIFIED="1609886821264">
+<node TEXT="No-brainer task that can directly be developed/executed by someone that kas the correct skill set" ID="ID_433237647" CREATED="1609887175247" MODIFIED="1609887233666"/>
+</node>
+</node>
+<node TEXT="LINKS" POSITION="left" ID="ID_1466257678" CREATED="1609887357357" MODIFIED="1609887361103">
+<edge COLOR="#00ff00"/>
+<node TEXT="Work breakdown structure" ID="ID_1595842665" CREATED="1609887363460" MODIFIED="1609887804139" LINK="https://www.workbreakdownstructure.com/"/>
+<node TEXT="Feature-driver development" ID="ID_1039850171" CREATED="1609887787911" MODIFIED="1609887796051" LINK="https://en.wikipedia.org/wiki/Feature-driven_development"/>
+<node TEXT="Dynamic systems development method" ID="ID_349958455" CREATED="1609887806908" MODIFIED="1609887888518" LINK="https://www.agilebusiness.org/page/TheDSDMAgileProjectFramework"/>
+</node>
+</node>
+</map>
